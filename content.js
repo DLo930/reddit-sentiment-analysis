7f452044ac52cdb42002cd378fb27c0021210983
DLo930 <dlo2@andrew.cmu.edu>
2019-05-25 19:27:00 -0400

    Edit permissions

--- content.js
+++ content.js
@@ -1,11 +1,6 @@
-function getColor() {
-
-}
-
 var elems = document.getElementsByClassName('Post');
 for(var i = 0; i < elems.length; i++) {
   elems[i].style.backgroundColor = 'blue';
-
 }
 
 var elems2 = document.getElementsByClassName('Comment');
@@ -13,17 +8,24 @@ for(var i = 0; i < elems2.length; i++) {
   elems2[i].style.backgroundColor = 'red';
 }
 
+var form = new FormData();
+form.append('text', 'Hello, world!');
+form.append('api_key', 'tgXXTl26MVLbrMgcoIsy8hGKNafAMutI1NdyXR4A9sU');
+form.append('lang_code', 'en');
+
 fetch('https://apis.paralleldots.com/v4/emotion/', {
   method: 'POST',
   headers: {
+    'Accept': 'application/json',
     'Content-Type': 'multipart/form-data'
   },
-  body: JSON.stringify({
+  body: form
+  JSON.stringify({
     text: 'Hello, world!',
     api_key: 'tgXXTl26MVLbrMgcoIsy8hGKNafAMutI1NdyXR4A9sU',
     lang_code: 'en'
   })
 })
-  .then(res => {
-    console.log(res.body);
+  .then(body => {
+    console.log(body);
   });
