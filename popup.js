83d9e9a27318a794e4565491ab3476aeca30605d
Austin Leung <austinle@andrew.cmu.edu>
2019-05-25 15:53:06 -0400

    Visit Reddit for a surprise & Click extension to see text change color

---
+++ popup.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var checkPageButton = document.getElementById('checkPage');
+  checkPageButton.addEventListener('click', function() {
+    checkPageButton.style.color = "blue";
+
+  }, false);
+}, false);
