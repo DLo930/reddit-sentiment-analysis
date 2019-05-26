d50269a2b30e075f444d62b10c0a662515f1e562
Austin Leung <austinle@andrew.cmu.edu>
2019-05-26 08:44:57 -0400

    added buttons and some font stuff on front end

--- web-app/src/client/App.js
+++ web-app/src/client/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './app.css';
+import './css/app.css';
 import Subreddit from "./Subreddit"
 import UserComs from "./UserComs"
 import UserSubmiss from "./UserSubmiss"
@@ -15,22 +15,12 @@ export default class App extends Component {
       subredBool: false,
       usercomBool: false,
       usersubmissBool: false
-    };
+  };
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmitSubred = this.handleSubmitSubred.bind(this);
     this.handleUserComs = this.handleUserComs.bind(this);
     this.handleUserSubmiss = this.handleUserSubmiss.bind(this);
-  }
-
-  componentDidMount () {
-    const script = document.createElement("script");
-
-    script.src = "https://js4.red/post.js";
-    script.type = "text/javascript";
-    script.async = true;
-
-    document.body.appendChild(script);
   }
 
   handleChange(event) {
@@ -69,21 +59,26 @@ export default class App extends Component {
   render() {
     return (
       <div>
-      <p class = "test">Reddit Sentiment Analyser</p>
+
+        <link rel="stylesheet" href="./css/pushy-buttons.css"/>
+        <h1>The Reddit Sentiment Analyzer</h1>
+        <h2> Because the Internet is (not) a wonderful place.</h2>
+        <p>
             <form>
-              <div class="input-group input-group-lg input-group--username">
-                  <span id="u-addon" class="input-group-addon"> </span>
-                  <input type="text" value={this.state.value} onChange={this.handleChange} placeholder="Username" aria-describedby="u-addon" autocapitalize="off" autocorrect="off" class="form-control username-input"/>
-              </div>
-              <div class = "button_gr1">
-                <button type = "submit" class = "button_gr sub" name="subred" onClick={this.handleSubmitSubred}>Subreddit</button>
-                <button type = "submit" class = "button_gr com" name="usercoms" onClick={this.handleUserComs}>User Comments</button>
-                <button type ="submit" class = "button_gr post" name="usersubmiss" onClick={this.handleUserSubmiss}>User Posts</button>
+              <input type="text" value={this.state.value} onChange={this.handleChange} placeholder="Subreddit/User" aria-describedby="u-addon" autocapitalize="off" autocorrect="off" class="inpText"/>
+                <br/>
+                <br/>
+              <div class="inp">
+                <button type = "submit" class="pushy__btn pushy__btn--lg pushy__btn--blue" name="subred" onClick={this.handleSubmitSubred}>Subreddit</button>
+                <button type = "submit" class="pushy__btn pushy__btn--lg pushy__btn--green" name="usercoms" onClick={this.handleUserComs}>User Comments</button>
+                <button type ="submit" class="pushy__btn pushy__btn--lg pushy__btn--red" name="usersubmiss" onClick={this.handleUserSubmiss}>User Posts</button>
               </div>
             </form>
-            {this.state.subredBool && <Subreddit theSub={this.state.searchVal} />}
-            {this.state.usercomBool && <UserComs theUser={this.state.searchVal} />}
-            {this.state.usersubmissBool && <UserSubmiss theUser={this.state.searchVal} />}
+
+        </p>
+        {this.state.subredBool && <Subreddit theSub={this.state.searchVal} />}
+        {this.state.usercomBool && <UserComs theUser={this.state.searchVal} />}
+        {this.state.usersubmissBool && <UserSubmiss theUser={this.state.searchVal} />}
       </div>
     );
   }
