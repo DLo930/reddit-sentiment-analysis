980373404092098162de99dc295e1d0681bcfd1d
Nwang1 <40575715+Nwang1@users.noreply.github.com>
2019-05-26 02:12:32 -0400

    CSS

--- web-app/src/client/App.js
+++ web-app/src/client/App.js
@@ -5,7 +5,18 @@ export default class App extends Component {
   render() {
     return (
       <div>
-        <h1>Title</h1>
+      <p class = "test">Reddit Analyser</p>
+            <div class="input-group input-group-lg input-group--username">
+              <span id="u-addon" class="input-group-addon">/u/</span>
+                <input type="text" placeholder="Username" aria-describedby="u-addon" autocapitalize="off" autocorrect="off" class="form-control username-input"/>
+                  <span class="input-group-btn">
+                    <button type="button" class="btn btn-secondary">Analyse</button>
+                    </span>
+                    <button type = "submit" class = "reddit">Reddit</button>
+                  <button type = "button" class = "twitter">Twitter</button>
+                  <button type ="button" class = "fb"> Facebook</button> 
+                </div>
+
       </div>
     );
   }
