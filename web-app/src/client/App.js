d2e2cf42ad7c7385f82d43bc01b7c0620149342c
Austin Leung <austinle@andrew.cmu.edu>
2019-05-26 02:07:29 -0400

    console log prints subreddit, comments, submissions

--- web-app/src/client/App.js
+++ web-app/src/client/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 import './app.css';
+import Subreddit from "./Subreddit"
+import UserComs from "./UserComs"
+import UserSubmiss from "./UserSubmiss"
 
 export default class App extends Component {
   render() {
     return (
       <div>
         <h1>Title</h1>
+        <Subreddit/>
+        <UserComs />
+        <UserSubmiss />
       </div>
     );
   }
