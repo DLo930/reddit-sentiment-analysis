ff652f35a160ee4dd1362eec37aa35d101656c2c
DLo930 <dlo2@andrew.cmu.edu>
2019-05-26 00:22:47 -0400

    Update title and favicon

--- web-app/src/server/index.js
+++ web-app/src/server/index.js
@@ -10,9 +10,10 @@ app.use(express.static('dist'));
 pd.apiKey = 'tgXXTl26MVLbrMgcoIsy8hGKNafAMutI1NdyXR4A9sU';
 
 app.post('/test', (req, res) => {
-  res.end({ test: "Success" });
+  res.end({ array: ["#FFFFFF"] });
 });
 
+// http://apis.paralleldots.com/text_docs/index.html#emotion
 app.post("/getColors", (req, res) => {
   console.log("Hello, world!");
   pd.emotionBatch(JSON.stringify(req.body.textArray), 'en')
@@ -21,7 +22,7 @@ app.post("/getColors", (req, res) => {
       res.end(res);
     }).catch((err) => {
       console.log(err);
-    })
+    });
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}!`));
